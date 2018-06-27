6257d8c7fe57def8f15ff5a0e7179cc11407786f
Nicholas Powers <njpowers7915@gmail.com>
2018-06-27 13:28:46 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,4 +11,7 @@ class StudentsController < ApplicationController
   def new
   end
 
+  def create
+      Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+
 end
